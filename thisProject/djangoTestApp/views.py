30501738e74e49a47a34a64f8da2d13a4bcd8f14
Augusto Neftali Ruiz Cauich <augusto.ruizcauich@gmail.com>
2017-05-25 14:51:40 -0500

    modificaciones  minimas

--- thisProject/djangoTestApp/views.py
+++ thisProject/djangoTestApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello worls')
+    return HttpResponse('<h1>Hello world</h1>')
